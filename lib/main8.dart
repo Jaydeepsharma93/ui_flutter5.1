84a9493066e003675d48126317a3e760a835a266
Jaydeep Sharma <143181361+Jaydeepsharma93@users.noreply.github.com>
2024-01-27 20:42:52 +0530

    Update main8.dart

--- lib/main8.dart
+++ lib/main8.dart
@@ -14,31 +14,80 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
-          body: Center(
-            child: Stack(
-              children: [
-                Container(
-                  alignment: Alignment(0,-4),
-                  height: 320,
-                  width: 320,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: Colors.orange,
-                      width: 30,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Center(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Stack(
+                      children: [
+                        Container(
+                          alignment: Alignment.bottomCenter,
+                          height: 320,
+                          width: 320,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: Colors.orange,
+                              width: 30,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 4,
+                          left: 20,
+                          child: Container(
+                            height: 260,
+                            width: 280,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.orange,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 70,
+                          left: 52,
+                          child: Container(
+                            height: 100,
+                            width: 100,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 70,
+                          right: 52,
+                          child: Container(
+                            height: 100,
+                            width: 100,
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  child: Container(
-                    height: 250,
-                    width: 250,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.orange,
-                    ),
+                ),
+              ),
+              AppBar(
+                backgroundColor: Colors.orange,
+                centerTitle: true,
+                title: const Text('Emoji',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
                   ),
-                )
-              ],
-            ),
+                ),
+                toolbarHeight: 70,
+              ),
+            ],
           ),
         ),
       ),
